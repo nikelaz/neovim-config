7b3e5feca0b74370e0fd917d10d37476f3205f02
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2026-05-01 07:28:06 +0300

    feat: autocomplete with lsp

--- lua/nikelaz/plugins/mason-lspconfig.lua
+++ lua/nikelaz/plugins/mason-lspconfig.lua
@@ -1,8 +1,17 @@
 return {
     "mason-org/mason-lspconfig.nvim",
-    opts = {},
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
         "neovim/nvim-lspconfig",
+        "hrsh7th/cmp-nvim-lsp",
     },
+    config = function()
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+        vim.lsp.config("*", {
+            capabilities = capabilities,
+        })
+
+        require("mason-lspconfig").setup()
+    end,
 }
