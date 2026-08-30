c1a9a869d468993aedfb48b6500e35cc0f3084a7
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2026-08-30 18:13:24 +0300

    feat: transparent neovim

--- lua/nikelaz/color_scheme.lua
+++ lua/nikelaz/color_scheme.lua
@@ -1,2 +1,17 @@
 vim.o.background = "dark"
+
+-- Transparent background: let the terminal's frosted-glass effect show through.
+-- Covers both old (vim.g) and new (setup{transparent_mode}) gruvbox.nvim APIs.
+vim.g.gruvbox_transparent_background = 1
+pcall(function()
+  local gruvbox = require("gruvbox")
+  if gruvbox.setup then
+    gruvbox.setup({ transparent_mode = true })
+  end
+end)
+
 vim.cmd([[colorscheme gruvbox]])
+
+-- Floating windows (telescope pickers, etc.) use NormalFloat; make them see-through too
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
